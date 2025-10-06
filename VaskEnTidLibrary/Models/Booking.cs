d5f83d3c11c6de6541bc0a8cc4cdda11e89f659f
mak025 <mak025@edu.zealand.dk>
2025-10-06 09:23:30 +0200

    Booking Class created

--- VaskEnTidLibrary/Models/Booking.cs
+++ VaskEnTidLibrary/Models/Booking.cs
@@ -8,5 +8,21 @@ namespace VaskEnTidLibrary.Models
 {
     public class Booking
     {
+        int BookingID { get; set; }
+        int TenantID { get; set; }
+        int MachineID { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+        string Status { get; set; }
+        public Booking(int bookingID, int tenantID, int machineID, DateTime startTime, DateTime endTime, string status)
+        {
+            BookingID = bookingID;
+            TenantID = tenantID;
+            MachineID = machineID;
+            StartTime = startTime;
+            EndTime = endTime;
+            Status = status;
+        }
+        public Booking() { }
     }
 }
