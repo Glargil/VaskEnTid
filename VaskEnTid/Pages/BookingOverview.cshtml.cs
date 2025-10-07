251cb99ac6662a71de4b354811c9eb89ef97cb58
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-10-07 10:18:44 +0200

    added page

---
+++ VaskEnTid/Pages/BookingOverview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VaskEnTid.Pages
+{
+    public class BookingOverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
