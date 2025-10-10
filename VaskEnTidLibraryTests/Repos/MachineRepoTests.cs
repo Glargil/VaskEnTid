d60fecbd23f83df40b985fb4a224dd30ce9f7ebf
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-10-10 10:04:16 +0200

    unittests

---
+++ VaskEnTidLibraryTests/Repos/MachineRepoTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VaskEnTidLibrary.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaskEnTidLibrary.Models.Machines;
+using System.Diagnostics;
+
+namespace VaskEnTidLibrary.Repos.Tests
+{
+    [TestClass()]
+    public class MachineRepoTests
+    {
+
+        [TestMethod()]
+        public void GetAllMachinesTest()
+        {
+            //Arrange
+            MachineRepo repo = new MachineRepo("Server=mssql.mkhansen.dk,1436;Database=Laundromat;User Id=sa;Password=Laundromat25;Encrypt=true;TrustServerCertificate=True;");
+            List<Machine> expectedMachines = new List<Machine>
+            {
+                new Washer(1),
+                new Washer(2),
+                new Washer(3),
+                new Dryer(4),
+                new Dryer(5),
+                new Dryer(6),
+                new Roller(7),
+                new Roller(8),
+                new Roller(9),
+            };
+            foreach (Machine expectedMachine in expectedMachines)
+            { Debug.WriteLine($"Id: {expectedMachine.MachineID}, Type: {expectedMachine.Type}"); }
+
+            //Act
+            List<Machine> actMachines = repo.GetAllMachines();
+            foreach (Machine actMachine in actMachines)
+            { Debug.WriteLine($"Id: {actMachine.MachineID}, Type: {actMachine.Type}"); }
+
+            //Assert
+            Assert.AreEqual(expectedMachines, actMachines);
+        }
+
+        [TestMethod()]
+        public void GetMachineByIdTest()
+        {
+            //Arrange
+            MachineRepo repo = new MachineRepo("Server=mssql.mkhansen.dk,1436;Database=Laundromat;User Id=sa;Password=Laundromat25;Encrypt=true;TrustServerCertificate=True;");
+            List<Machine> allMachines = repo.GetAllMachines();
+            Machine expectedMachine = new Washer(1);
+
+            //Act
+            Machine actMachine = repo.GetMachineById(1);
+
+            //Assert
+            Assert.AreEqual(expectedMachine.MachineID, actMachine.MachineID);
+        }
+
+    }
+}
